7ecb27b6d0c6f72f6482624b2f4bfdcea6d02260
SaiSanthoshKumarPokala <saisanthoshkumarpokala@gmail.com>
2025-07-02 14:10:28 +0530

    Firebase project changed to CampusConnect from IOMP

---
+++ firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRNABRIlU92-jEQY9jfYN-IJo3TvqbvsU",
+  authDomain: "campusconnect-iomp.firebaseapp.com",
+  projectId: "campusconnect-iomp",
+  storageBucket: "campusconnect-iomp.firebasestorage.app",
+  messagingSenderId: "94609202390",
+  appId: "1:94609202390:web:f7c7238bb2069416993d77",
+  measurementId: "G-Z6RXQB1D06"
+};
+
+// Initialize Firebase
+export const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
